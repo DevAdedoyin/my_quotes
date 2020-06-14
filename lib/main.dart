dea5622a4831d34818044ef4ee30590c111ab1a2
Adedoyin <isradeoluwa@gmail.com>
2020-06-14 03:38:17 +0100

    Card added for each quote and its author

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/painting.dart';
 import 'package:myquotes/quotes.dart';
 
 void main() => runApp(MaterialApp(home: Quotes(),));
@@ -12,19 +13,37 @@ class Quotes extends StatefulWidget {
 class _QuotesState extends State<Quotes> {
 
   List<MyQuotes> quotes = [
-    MyQuotes(author: "Adedoyin", quote: 'Be who you are and say what you feel, because those who mind don’t matter, and those who matter don’t mind\n'),
-    MyQuotes(author: "Adedoyin", quote:  'Be yourself everyone else is already taken.\n'),
-    MyQuotes(author: "Adedoyin", quote:  'Insanity is doing the same thing, over and over again, but expecting different results.\n'),
-    MyQuotes(author: "Adedoyin", quote:  'I\’ve learned that people will forget what you said, people will forget what you did, but people will never forget how you made them feel.\n'),
-    MyQuotes(author: "Adedoyin", quote:  'To be yourself in a world that is constantly trying to make you something else is the greatest accomplishment.'),
+    MyQuotes(author: "~Adedoyin", quote: 'Be who you are and say what you feel, because those who mind don’t matter, and those who matter don’t mind\n'),
+    MyQuotes(author: "~Adedoyin", quote:  'Be yourself everyone else is already taken.\n'),
+    MyQuotes(author: "~Adedoyin", quote:  'Insanity is doing the same thing, over and over again, but expecting different results.\n'),
+    MyQuotes(author: "~Adedoyin", quote:  'I\’ve learned that people will forget what you said, people will forget what you did, but people will never forget how you made them feel.\n'),
+    MyQuotes(author: "~Adedoyin", quote:  'To be yourself in a world that is constantly trying to make you something else is the greatest accomplishment.'),
   ];
+
+  Widget quoteTemplate(quote){
+    return Card(
+      color: Colors.grey[500],
+      margin: EdgeInsets.fromLTRB(15, 10, 15, 10),
+      child: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: <Widget>[
+            Text(quote.quote, style: TextStyle(color: Colors.white, fontStyle: FontStyle.normal, fontWeight: FontWeight.bold),),
+            Text(quote.author, style: TextStyle(color: Colors.white70, fontStyle: FontStyle.italic, fontWeight: FontWeight.bold),),
+          ],
+        ),
+      ),
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.grey[800],
       appBar: AppBar(title: Text("My Favorite Quotes", style: TextStyle(fontWeight: FontWeight.bold),), backgroundColor: Colors.blueGrey[500], centerTitle: true,),
     body: Column(
-      children: quotes.map((quote) => Text('${quote.quote} - ${quote.author}')).toList(),
+      children: quotes.map((quote) => quoteTemplate(quote)).toList(),
     ),
     );
   }
